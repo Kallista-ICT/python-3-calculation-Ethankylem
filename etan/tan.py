dda9a6593dfc5b7fe5d5dc2f620ad83d0c088f13
Ethankylem <ethanmarciano065@gmail.com>
2025-02-12 01:49:09 +0000

    submit

---
+++ etan/tan.py
@@ -0,0 +1,8 @@
+distance = float(input("enter the distance (in meters): "))
+time = float(input("enter the time(in seconds): "))
+
+# Comment here
+velocity = distance / time
+
+# Comment here 
+print(f"the velocity is {velocity:.2f} m/s.")
